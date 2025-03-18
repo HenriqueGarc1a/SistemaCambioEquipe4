1c2886649b4d439272abe7cd7e1ecb0e859927c6
Henrique Garcia <henriquegarcia1597@gmail.com>
2025-03-18 15:04:50 -0300

    js pela metade

--- script.js
+++ script.js
@@ -1,22 +1,115 @@
+valores = []
+
 async function getCotacao() {
     try {
-      const response = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL?token=986495aa4c64154c4c74a5dad7b5949ff8968fed7528dff87d931c7843a67ba0");
+      
+      const response = await fetch("https://economia.awesomeapi.com.br/json/last/BRL-USD,JPY-USD,CAD-USD,ARS-USD,AUD-USD,GBP-USD,KRW-USD,EUR-USD,CHF-USD?token=986495aa4c64154c4c74a5dad7b5949ff8968fed7528dff87d931c7843a67ba0");
   
       if (!response.ok) {
         throw new Error(`Erro HTTP! Status: ${response.status}`);
       }
-  
       const data = await response.json();
-      t = data.USDBRL.ask
+      setCambios(data)
 
-      console.log("Dados recebidos:", t);
-      
     } catch (error) {
       console.error("Erro ao buscar a taxa de câmbio:", error);
     }
+
+   
+  
+}
+
+function setCambios(data){
+
+  valores.push(eval(data.ARSUSD.bid))
+  valores.push(eval(data.AUDUSD.bid))
+  valores.push(eval(data.BRLUSD.bid))
+  valores.push(eval(data.CHFUSD.bid))
+  valores.push(eval(data.CADUSD.bid))
+  valores.push(eval(data.EURUSD.bid))
+  valores.push(eval(data.GBPUSD.bid))
+  valores.push(eval(data.JPYUSD.bid))
+  valores.push(eval(data.KRWUSD.bid))
+  valores.push(1)
+  
+}
+
+function getCambio(){
+  
+     select = document.getElementById("opcoes1");
+     entrada1 = select.selectedIndex
+
+     select = document.getElementById("opcoes2");
+     entrada2 = select.selectedIndex
+
+     x1= document.getElementById("x1").value;
+     
+     
+      z=(valores[entrada1]*x1)/(valores[entrada2]);
+
+     // z = z.toFixed(2)
+    
+    document.getElementById("saida").innerHTML = getPfix(entrada1)+" 1.00 "+" = "+getPfix(entrada2)+" "+z;
+
+   
+}
+
+
+function getPfix(x){
+
+  switch(x){
+
+    case 0:
+      return "ARS";
+    break;
+
+    case 1:
+      return "AUD";
+    break;
+
+    case 2:
+      return "BRL";
+    break;
+
+    case 3:
+      return "CHF";
+    break;
+
+    case 4:
+      return "CAD";
+    break;
+
+    case 5:
+      return "EUR";
+    break;
+
+    case 6:
+      return "GBP";
+    break;
+
+    case 7:
+      return "JPY";
+    break;
+
+    case 8:
+      return "KRW";
+    break;
+
+    case 9:
+      return "USD";
+    break;
+
   }
 
 
 
+}
+
+
   
   getCotacao();
+
+ 
+
+
+  
